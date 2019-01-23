92c1fecf0f56fcd1ea658a3f0ae56c444a493212
Ola Bognø <olarbogn@gmail.com>
2019-01-23 11:23:43 +0100

    First publish prototype

---
+++ TD_Ver4/Assets/EnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    private float timeBetweenWaves = 5f;
+    private float countdown = 1f;
+
+    public Text waveCountDownText;
+
+    private int waveIndex = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave()); // Uses StartCoroutine because of the IEnumerator method
+            countdown = timeBetweenWaves;
+            timeBetweenWaves++;
+        }
+
+        
+        countdown -= Time.deltaTime;
+        waveCountDownText.text = "WaveCountdown: " + Mathf.Round(countdown + 1).ToString();
+    }
+
+    // IEnumerator allows pausing of the subroutine "SpawnWave()"
+    IEnumerator SpawnWave() {
+        waveIndex++;
+        //Debug.Log("Wave Incoming!")
+        for (int i = 0; i < waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ TD_Ver4/Assets/Turret.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform PartToRotate;
+    public float turnSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.3f); //Checks target at start and every 0.5s
+    }
+
+    void UpdateTarget() 
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies) {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy <= shortestDistance) {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range) {
+            target = nearestEnemy.transform;
+        } else {
+            target = null;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) 
+            return; //if turret doesnt have a target cancel update
+
+        Vector3 dir = target.position - transform.position; // vector pointing to enemy
+        Quaternion lookRotation = Quaternion.LookRotation(dir); // converts dir into Quaternion
+
+        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles; // sets lookrotation to be in eulerAngles
+        
+        // Makes shure that the tower object only turns in the correct dimensions. TODO: FIX THE BLOODY THINGY!!!
+        //PartToRotate.rotation = Quaternion.Euler(0f, 0f, rotation.z); // rotates PartToRotate to
+        //Quaternion.Lerp() smooths transitions between angles by speedparameter "turnSpeed"
+        
+        PartToRotate.rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
