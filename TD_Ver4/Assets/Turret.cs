8a1659e4e461c0b0075b49c9274791382957d5dd
Ola Bognø <olarbogn@gmail.com>
2019-01-23 13:50:30 +0100

    Fixed rotationscript on turret

--- TD_Ver4/Assets/Turret.cs
+++ TD_Ver4/Assets/Turret.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-
     private Transform target;
     public float range = 15f;
 
     public string enemyTag = "Enemy";
+    private GameObject nearestEnemy;
 
     public Transform PartToRotate;
     public float turnSpeed = 10f;
@@ -16,16 +14,16 @@ public class Turret : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("UpdateTarget", 0f, 0.3f); //Checks target at start and every 0.5s
+        InvokeRepeating("UpdateTarget", 0f, 0.1f); //Checks target at start and every 0.5s
     }
 
     void UpdateTarget() 
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
-        GameObject nearestEnemy = null;
-
-        foreach(GameObject enemy in enemies) {
+        nearestEnemy = null;
+        
+        foreach (GameObject enemy in enemies) {
             float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
             if (distanceToEnemy <= shortestDistance) {
                 shortestDistance = distanceToEnemy;
@@ -33,7 +31,7 @@ public class Turret : MonoBehaviour
             }
         }
 
-        if(nearestEnemy != null && shortestDistance <= range) {
+        if(nearestEnemy != null && shortestDistance <= range)  {
             target = nearestEnemy.transform;
         } else {
             target = null;
@@ -47,17 +45,10 @@ public class Turret : MonoBehaviour
         if (target == null) 
             return; //if turret doesnt have a target cancel update
 
-        Vector3 dir = target.position - transform.position; // vector pointing to enemy
-        Quaternion lookRotation = Quaternion.LookRotation(dir); // converts dir into Quaternion
-
-        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles; // sets lookrotation to be in eulerAngles
-        
-        // Makes shure that the tower object only turns in the correct dimensions. TODO: FIX THE BLOODY THINGY!!!
-        //PartToRotate.rotation = Quaternion.Euler(0f, 0f, rotation.z); // rotates PartToRotate to
-        //Quaternion.Lerp() smooths transitions between angles by speedparameter "turnSpeed"
-        
-        PartToRotate.rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed);
-
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed);
     }
 
     void OnDrawGizmosSelected() {
