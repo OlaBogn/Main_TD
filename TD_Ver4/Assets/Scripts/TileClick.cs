ea9bb9a1149dacfafa88c11fe411a69327d62f58
OlaBogn <OlaBogn@users.noreply.github.com>
2019-02-12 11:20:57 +0100

    Fixed bug with TurretSpawner 2

--- TD_Ver4/Assets/Scripts/TileClick.cs
+++ TD_Ver4/Assets/Scripts/TileClick.cs
@@ -7,13 +7,16 @@ public class TileClick : MonoBehaviour
     public GameObject turret;
     public Transform spawnPoint;
 
+    private bool hasTurret = false;
+
     void Start() {
         spawnPoint = transform;
     }
 
     void OnMouseDown() {
-        if (this.gameObject.CompareTag(tileTag)) {
+        if (this.gameObject.CompareTag(tileTag) && hasTurret == false) {
             Instantiate(turret, spawnPoint.position, spawnPoint.rotation);
+            hasTurret = true;
         }
     }
 }
