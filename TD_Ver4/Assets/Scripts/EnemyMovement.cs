7f8b307e4008a4c14395705642abb57c573cf72a
OlaBogn <46599056+OlaBogn@users.noreply.github.com>
2019-02-26 16:55:49 +0100

    Revert "Merge pull request #14 from OlaBogn/Dosh-GUI"
    This reverts commit bd06a7e38d74352095f1cbe830882d749ba18f0d, reversing
    changes made to 1d5eb1b6a805691418d6dd95628339f1f90e9791.

--- TD_Ver4/Assets/Scripts/EnemyMovement.cs
+++ TD_Ver4/Assets/Scripts/EnemyMovement.cs
@@ -7,21 +7,28 @@ public class EnemyMovement : MonoBehaviour {
 
     private Transform target;
     private int wayPointIndex = 0;
-    private GameObject go;
+    public GameObject pivot;
+    public GameObject sprite;
+
+    
     
     void Start() {
         target = Waypoints.points[0];
     }
-    
+    private void Awake() {
+        sprite.transform.rotation = Quaternion.Euler(0, 90, 90);
+    }
+
     void Update() {
+        // moves enemy
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, target.position) <= 0.1f) {
             GetNextWaypoint();
         }
-
-
+        
+        pivot.transform.rotation = Quaternion.LookRotation(dir);
     }
 
     void GetNextWaypoint() {
@@ -34,6 +41,5 @@ public class EnemyMovement : MonoBehaviour {
         wayPointIndex++;    
         target = Waypoints.points[wayPointIndex];
     }
-    
     
 }
