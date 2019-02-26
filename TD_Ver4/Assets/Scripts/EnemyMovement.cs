0a4e265c8c0a9e768bdeaec8780f2e613b0c3fc3
OlaBogn <OlaBogn@users.noreply.github.com>
2019-02-26 11:18:38 +0100

    Changed EnemyPrefabs
    Now has base, pivot and visual sprite ontop.

--- TD_Ver4/Assets/Scripts/EnemyMovement.cs
+++ TD_Ver4/Assets/Scripts/EnemyMovement.cs
@@ -7,21 +7,21 @@ public class EnemyMovement : MonoBehaviour {
 
     private Transform target;
     private int wayPointIndex = 0;
-    private GameObject go;
+    public GameObject pivot;
     
     void Start() {
         target = Waypoints.points[0];
     }
     
     void Update() {
+        // moves enemy
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, target.position) <= 0.1f) {
             GetNextWaypoint();
         }
-
-
+        pivot.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
     }
 
     void GetNextWaypoint() {
@@ -34,6 +34,5 @@ public class EnemyMovement : MonoBehaviour {
         wayPointIndex++;    
         target = Waypoints.points[wayPointIndex];
     }
-    
     
 }
