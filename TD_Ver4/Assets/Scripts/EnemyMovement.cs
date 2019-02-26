b4af95365a929b0ad2fc93008734672d24b889b2
OlaBogn <OlaBogn@users.noreply.github.com>
2019-02-26 12:32:35 +0100

    Fixed enemyRotation
    added lookdirection while moving and offset on awake for sprites

--- TD_Ver4/Assets/Scripts/EnemyMovement.cs
+++ TD_Ver4/Assets/Scripts/EnemyMovement.cs
@@ -8,11 +8,17 @@ public class EnemyMovement : MonoBehaviour {
     private Transform target;
     private int wayPointIndex = 0;
     public GameObject pivot;
+    public GameObject sprite;
+
+    
     
     void Start() {
         target = Waypoints.points[0];
     }
-    
+    private void Awake() {
+        sprite.transform.rotation = Quaternion.Euler(0, 90, 90);
+    }
+
     void Update() {
         // moves enemy
         Vector3 dir = target.position - transform.position;
@@ -21,7 +27,8 @@ public class EnemyMovement : MonoBehaviour {
         if (Vector3.Distance(transform.position, target.position) <= 0.1f) {
             GetNextWaypoint();
         }
-        pivot.transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+        
+        pivot.transform.rotation = Quaternion.LookRotation(dir);
     }
 
     void GetNextWaypoint() {
