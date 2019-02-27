803a0a97533c4792f4fcc0b943c39c1c4da36326
OlaBogn <OlaBogn@users.noreply.github.com>
2019-02-27 12:58:55 +0100

    Fixed to a working build on android

---
+++ TD_Ver4/Assets/Scripts/LevelWaveList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelWaveList : MonoBehaviour
+{
+    
+    public static string[] GetWaveList() {
+        string[] x = {
+            "3,3,3,3,3,3",
+            "0,0,0,0",
+            "0,0,0,1",
+            "0,0,1,1",
+            "0,1,1,1",
+            "1,1,1,1",
+            "3,3,3,3,3,3"
+        };
+        return x;
+
+
+    }
+}
