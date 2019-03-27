8e914b67b7893bd0757b025ba61a3a7be8e1875c
OlaBogn <OlaBogn@users.noreply.github.com>
2019-03-27 13:56:18 +0100

    Continued on implenting turret select

---
+++ TD_Ver4/Assets/GameController/TurretSelection/SelectTurret.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectTurret : MonoBehaviour
+{
+    private int next = 0;
+
+    [Header("Unity setup")]
+    public GameObject[] turrets; // remembers all turret prefabs
+    public Text[] buttons; // collects all buttons to modify content
+    public GameObject[] selectedTurrets; // shows selected turrets
+
+    private void Awake() {
+        ChangeButtonNames();
+    }
+
+    // TODO: attach prefab to TurretSelected on button clicked
+
+    public void ChangeButtonNames() {
+        int count = 0;
+        foreach(GameObject turret in turrets) {
+            buttons[count].text = turret.name;
+            count++;
+        }
+    }
+
+
+}
