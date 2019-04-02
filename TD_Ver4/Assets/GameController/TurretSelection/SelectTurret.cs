a117042bf95fadc264b16b9ee3f027e1239f67f5
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-02 11:53:16 +0200

    Keeping on going on

--- TD_Ver4/Assets/GameController/TurretSelection/SelectTurret.cs
+++ TD_Ver4/Assets/GameController/TurretSelection/SelectTurret.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SelectTurret : MonoBehaviour
 {
-    private int next = 0;
+    private int count = 0;
+    private int maxCount = 5;
 
     [Header("Unity setup")]
     public GameObject[] turrets; // remembers all turret prefabs
+    public Sprite[] sprites; // remembers displaySprite for each prefab
     public Text[] buttons; // collects all buttons to modify content
     public GameObject[] selectedTurrets; // shows selected turrets
-
+    
     private void Awake() {
         ChangeButtonNames();
     }
@@ -21,10 +22,73 @@ public class SelectTurret : MonoBehaviour
     public void ChangeButtonNames() {
         int count = 0;
         foreach(GameObject turret in turrets) {
+            if (turret == null)
+                break;
             buttons[count].text = turret.name;
             count++;
         }
     }
 
+    public void UpdateSprite(int n) {
+        selectedTurrets[count].GetComponent<SpriteRenderer>().sprite = sprites[n];
+        
+    }
 
+    public void ChooseTurret() {
+        string btnClicked = EventSystem.current.currentSelectedGameObject.name; // Gets the name of clicked button
+        Debug.Log("Count = " + count + " Button pressed: " + btnClicked);
+        
+        // Checks if player has selected maximum amount of turrets
+        if (count >= maxCount) {
+            Debug.Log("Tried to exceed maxCount variable.");
+            return;
+        }
+
+        switch (btnClicked) {
+            case "btn0":
+                selectedTurrets[count] = turrets[0];
+                UpdateSprite(0);
+                break;
+            case "btn1":
+                selectedTurrets[count] = turrets[1];
+                UpdateSprite(1);
+                break;
+            case "btn2":
+                selectedTurrets[count] = turrets[2];
+                UpdateSprite(2);
+                break;
+            case "btn3":
+                selectedTurrets[count] = turrets[3];
+                UpdateSprite(3);
+                break;
+            case "btn4":
+                selectedTurrets[count] = turrets[4];
+                UpdateSprite(4);
+                break;
+            case "btn5":
+                selectedTurrets[count] = turrets[5];
+                UpdateSprite(5);
+                break;
+            case "btn6":
+                selectedTurrets[count] = turrets[6];
+                UpdateSprite(6);
+                break;
+            case "btn7":
+                selectedTurrets[count] = turrets[7];
+                UpdateSprite(7);
+                break;
+            case "btn8":
+                selectedTurrets[count] = turrets[8];
+                UpdateSprite(8);
+                break;
+            case "btn9":
+                selectedTurrets[count] = turrets[9];
+                UpdateSprite(9);
+                break;
+            default:
+                Debug.Log("Couldnt find turretName");
+                break;
+        }
+        count++;
+    }
 }
