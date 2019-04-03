837a14366c34b82590c476a98cd63b89714f066d
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-03 11:44:41 +0200

    Bugfixes and update buttons in levelselect on start

---
+++ TD_Ver4/Assets/UI_Prefabs/TurretSelect/UpdateButtonNames.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateButtonNames : MonoBehaviour
+{
+
+    public GameObject[] btnsSelected;
+
+    private void Start() {
+        for (int i = 0; i < btnsSelected.Length; i++) {
+            GameObject go = btnsSelected[i].transform.GetChild(0).gameObject;
+            go.GetComponent<Text>().text = GameControl.control.prefabs[i].name;
+        }
+    }
+}
