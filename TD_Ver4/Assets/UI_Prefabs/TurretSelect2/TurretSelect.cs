3c39786e80b21349cc280730e93cb5aba16ded97
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-15 09:41:52 +0200

    made clear functionality and automatic updating of button content

--- TD_Ver4/Assets/UI_Prefabs/TurretSelect2/TurretSelect.cs
+++ TD_Ver4/Assets/UI_Prefabs/TurretSelect2/TurretSelect.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class TurretSelect : MonoBehaviour
 {
     [Header("Unity setup")]
+    public Sprite emptyPlaceholder;
     public GameObject[] turrets = new GameObject[10];
 
     [Header("Script defined refferences")]
@@ -17,20 +18,27 @@ public class TurretSelect : MonoBehaviour
     public Sprite[] baseSprite;
     public Sprite[] topSprite;
 
-    private int counter = 0;
-
-    // Start is called before the first frame update
+    public bool[] selectedSlotsOccupied;
+    
+    // TODO: make buttons remember their contents and not update with contents if deselected
+    
     void Start()
     {
         UpdateTurretButtons();
     }
     
     void UpdateTurretButtons() {
+        
         // child(0) = TurretButtons, child(1) = TurretsSelected, child(2) = InfoPanels
         turretButtons = new GameObject[gameObject.transform.GetChild(0).transform.childCount]; // children of TurretButtons
         infoPanels = new GameObject[gameObject.transform.GetChild(2).transform.childCount]; // children of InfoPanels
         turretSelectedButtons = new GameObject[gameObject.transform.GetChild(1).transform.childCount]; // children of TurretsSelected
-        
+
+        baseSprite = new Sprite[turrets.Length];
+        topSprite = new Sprite[turrets.Length];
+
+        selectedSlotsOccupied = new bool[turretSelectedButtons.Length];
+
         // Setts up turretButtons & infoPanel array references
         for (int i = 0; i < turretButtons.Length; i++) {
             turretButtons[i] = gameObject.transform.GetChild(0).transform.GetChild(i).gameObject;
@@ -39,12 +47,20 @@ public class TurretSelect : MonoBehaviour
 
         // Setts up baseSprite array
         for (int i = 0; i < turrets.Length; i++) {
+            baseSprite[i] = turrets[i].GetComponent<SpriteRenderer>().sprite;
+        }
+
+        // Setts up topSprite array
+        for (int i = 0; i < turrets.Length; i++) {
             topSprite[i] = turrets[i].transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
         }
 
+        
+        // writes text- and image- elements onto turretButtons
         for (int i = 0; i < turrets.Length; i++) {
             turretButtons[i].transform.GetChild(0).GetComponent<Text>().text = turrets[i].name;
-            turretButtons[i].transform.GetChild(1).GetComponent<Image>().sprite = topSprite[i]; //turrets[i].transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite
+            turretButtons[i].transform.GetChild(2).GetComponent<Image>().sprite = topSprite[i];
+            turretButtons[i].transform.GetChild(1).GetComponent<Image>().sprite = baseSprite[i];
         }
 
         // Setts up turretSelectedButtons array refferences
@@ -52,51 +68,109 @@ public class TurretSelect : MonoBehaviour
             turretSelectedButtons[i] = gameObject.transform.GetChild(1).transform.GetChild(i).gameObject;
         }
 
-        
-
+        // Setts up turretSelectedButtons
         for (int i = 0; i < turretSelectedButtons.Length; i++) {
-            turretSelectedButtons[i].transform.GetChild(0).GetComponent<Text>().text = GameControl.control.prefabs[i].name;
-            turretSelectedButtons[i].transform.GetChild(1).GetComponent<Image>().sprite = GameControl.control.prefabs[i].transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+            for (int n = 0; n < turrets.Length; n++) {
+                if (GameControl.control.prefabs[i].name == turrets[n].name) {
+                    turretSelectedButtons[i].transform.GetChild(0).GetComponent<Text>().text = turrets[n].name;
+                    turretSelectedButtons[i].transform.GetChild(1).GetComponent<Image>().sprite = baseSprite[n];
+                    turretSelectedButtons[i].transform.GetChild(2).GetComponent<Image>().sprite = topSprite[n];
+                }
+            }
+        }
+
+        // Setts up selectedSlotsOccupied array
+        for (int i = 0; i < selectedSlotsOccupied.Length; i++) {
+            selectedSlotsOccupied[i] = true;
         }
     }
 
     public void SelectTurretClicked() {
+        
         string turretName = EventSystem.current.currentSelectedGameObject.gameObject.transform.GetChild(0).GetComponent<Text>().text;
-        Debug.Log(turretName);
+        int counter = 0;
 
+        for (int i = 0; i < selectedSlotsOccupied.Length; i++) {
+            if (selectedSlotsOccupied[i]) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        if (counter > selectedSlotsOccupied.Length - 1) {
+            Debug.Log("All slots occupied");
+            return;
+        }
+        
         //TODO: set each turret into count position
         if (turretName == turrets[0].name) {
             GameControl.control.prefabs[counter] = turrets[0];
-            turretSelectedButtons[counter].transform.GetChild(0).GetComponent<Text>().text = turrets[0].name; //TODO: complete this statement on rest of alternatives;
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[1].name) {
             GameControl.control.prefabs[counter] = turrets[1];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[2].name) {
             GameControl.control.prefabs[counter] = turrets[2];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[3].name) {
             GameControl.control.prefabs[counter] = turrets[3];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[4].name) {
             GameControl.control.prefabs[counter] = turrets[4];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[5].name) {
             GameControl.control.prefabs[counter] = turrets[5];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[6].name) {
             GameControl.control.prefabs[counter] = turrets[6];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[7].name) {
             GameControl.control.prefabs[counter] = turrets[7];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[8].name) {
             GameControl.control.prefabs[counter] = turrets[8];
-            counter++;
+            RefreshSelectedTurrets();
         } else if (turretName == turrets[9].name) {
             GameControl.control.prefabs[counter] = turrets[9];
-            counter++;
+            RefreshSelectedTurrets();
+        }
+    }
+
+    void RefreshSelectedTurrets() {
+        int counter = 0;
+        for (int i = 0; i < turretSelectedButtons.Length; i++) {
+            for (int n = 0; n < turrets.Length; n++) {
+                if (GameControl.control.prefabs[i].name == turrets[n].name) {
+                    turretSelectedButtons[i].transform.GetChild(0).GetComponent<Text>().text = turrets[n].name;
+                    turretSelectedButtons[i].transform.GetChild(1).GetComponent<Image>().sprite = baseSprite[n];
+                    turretSelectedButtons[i].transform.GetChild(2).GetComponent<Image>().sprite = topSprite[n];
+                }
+            }
+        }
+        Debug.Log("Refreshed");
+        counter++;
+    }
+
+    public void ClearSelectedTurret() {
+        string selectedTurretName = EventSystem.current.currentSelectedGameObject.gameObject.transform.GetChild(0).GetComponent<Text>().text;
+        if (selectedTurretName == "Empty") {
+            Debug.Log("Slot is empty");
+            return;
         }
 
+        GameObject temp = null;
+
+        for (int i = 0; i < turretSelectedButtons.Length; i++) {
+            if (turretSelectedButtons[i].transform.GetChild(0).GetComponent<Text>().text == selectedTurretName) {
+                temp = turretSelectedButtons[i];
+                selectedSlotsOccupied[i] = false;
+                break;
+            }
+        }
+
+        temp.transform.GetChild(0).GetComponent<Text>().text = "Empty";
+        temp.transform.GetChild(1).GetComponent<Image>().sprite = emptyPlaceholder;
+        temp.transform.GetChild(2).GetComponent<Image>().sprite = emptyPlaceholder;
     }
 }
