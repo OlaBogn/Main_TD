dc50a7201e86adb1a6366abdd21617189cf5cedc
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-23 11:30:56 +0200

    Added all enemyprefabs to enemyspawner and fixed boss bug

---
+++ TD_Ver4/Assets/BossMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Transform target;
+    private int wayPointIndex = 0;
+    public GameObject pivot;
+    public GameObject sprite;
+
+    void Start() {
+        target = Waypoints.points[0];
+    }
+    private void Awake() {
+        sprite.transform.rotation = Quaternion.Euler(0, 90, 90);
+    }
+
+    void Update() {
+
+        // moves enemy
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWaypoint();
+        }
+
+        pivot.transform.rotation = Quaternion.LookRotation(dir);
+    }
+
+    void GetNextWaypoint() {
+        if (wayPointIndex >= Waypoints.points.Length - 1) {
+            GameObject go = GameObject.FindGameObjectWithTag("GameMaster");
+
+            go.SendMessage("DamagePlayer", null);
+
+            Destroy(gameObject);
+
+            return;
+        }
+
+        wayPointIndex++;
+        target = Waypoints.points[wayPointIndex];
+    }
+}
