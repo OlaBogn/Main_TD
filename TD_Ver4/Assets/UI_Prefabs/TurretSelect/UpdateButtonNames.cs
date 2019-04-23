5e71e514ce71f339862937e8bd8cf34b3cb6f667
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-23 10:28:29 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/OlaBogn/Main_TD""
    This reverts commit 3f224cb3925aa3b75c5280234c08007a558e18aa.

--- TD_Ver4/Assets/UI_Prefabs/TurretSelect/UpdateButtonNames.cs
+++ TD_Ver4/Assets/UI_Prefabs/TurretSelect/UpdateButtonNames.cs
@@ -7,11 +7,48 @@ public class UpdateButtonNames : MonoBehaviour
 {
 
     public GameObject[] btnsSelected;
+    public Sprite[] turretSprites;
 
     private void Start() {
         for (int i = 0; i < btnsSelected.Length; i++) {
             GameObject go = btnsSelected[i].transform.GetChild(0).gameObject;
             go.GetComponent<Text>().text = GameControl.control.prefabs[i].name;
+            go = btnsSelected[i].transform.GetChild(1).gameObject;
+            go.GetComponent<Image>().sprite = FindSprite(GameControl.control.prefabs[i].name);
         }
+    }
+
+    public Sprite FindSprite(string name) {
+        switch (name.Substring(0,3)) {
+            case ("Fir"):
+                return turretSprites[0];
+               
+            case ("Gat"):
+                return turretSprites[1];
+                
+            case ("Las"):
+                return turretSprites[2];
+                
+            case ("Mis"):
+                return turretSprites[3];
+                
+            case ("Pow"):
+                return turretSprites[4];
+                
+            case ("Rai"):
+                return turretSprites[5];
+                
+            case ("Sli"):
+                return turretSprites[6];
+                
+            case ("Sni"):
+                return turretSprites[7];
+                
+            default:
+                Debug.Log("Sprite not found");
+                return null;
+
+        }
+
     }
 }
