8bb0a12b493831f9331042afd3a0cd1abb8c80f3
OlaBogn <OlaBogn@users.noreply.github.com>
2019-04-28 14:42:01 +0200

    Added prices to turretButtons

--- TD_Ver4/Assets/Scripts/Shop.cs
+++ TD_Ver4/Assets/Scripts/Shop.cs
@@ -12,6 +12,16 @@ public class Shop : MonoBehaviour {
 
     private GameObject gm;
 
+    private int gattling = 105;
+    private int laser = 110;
+    private int missile = 150;
+    private int fire = 90;
+    private int powershot = 120;
+    private int railgun = 135;
+    private int slime = 105;
+    private int sniper = 140;
+    private int nukethrower = 300;
+    private int artilleri = 130;
 
     private void Start() {
         gm = GameObject.FindGameObjectWithTag("GameMaster");
@@ -23,6 +33,38 @@ public class Shop : MonoBehaviour {
         for (int i = 0; i < turretBuildButtons.Length; i++) {
             GameObject temp = turretBuildButtons[i].transform.GetChild(0).gameObject;
             temp.GetComponent<Text>().text = GameControl.control.prefabs[i].name;
+
+            if (GameControl.control.prefabs[i].name.Substring(0,3) == "Sni") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + sniper;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Gat") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + gattling;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Art") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + artilleri;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Fir") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + fire;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Las") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + laser;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Mis") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + missile;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Nuk") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + nukethrower;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Pow") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + powershot;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Rai") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + railgun;
+            } else if (GameControl.control.prefabs[i].name.Substring(0, 3) == "Sli") {
+                Text txt = turretBuildButtons[i].transform.GetChild(1).gameObject.GetComponent<Text>();
+                txt.text = "Cost: " + slime;
+            } 
         }
     }
 
